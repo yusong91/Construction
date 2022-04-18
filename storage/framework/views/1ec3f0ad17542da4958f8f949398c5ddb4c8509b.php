3ce0c877726ebb0fc0958e9a5dfb5f2567ccc85d
HOUN YOUSONG <yusonghoun91@gmail.com>
2022-04-18 09:27:38 +0700

    upload

---
+++ storage/framework/views/1ec3f0ad17542da4958f8f949398c5ddb4c8509b.php
@@ -0,0 +1,5 @@
+<link media="all"
+      type="text/css"
+      rel="stylesheet"
+      href="<?php echo e(url(mix('/css/announcements.css', 'vendor/plugins/announcements'))); ?>">
+<?php /**PATH /Users/yusonghoun/Web Software/construction/vendor/vanguardapp/announcements/src/../resources/views/partials/styles.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/3ff5502aa0ac190c3cce0079561a4ef371d8ebd7.php
@@ -0,0 +1,5 @@
+<script>
+    var read_announcements_endpoint = "<?php echo e(route('announcements.read')); ?>";
+</script>
+<script src="<?php echo e(url("vendor/plugins/announcements/js/announcements.js")); ?>"></script>
+<?php /**PATH /Users/yusonghoun/Web Software/construction/vendor/vanguardapp/announcements/src/../resources/views/partials/scripts.blade.php ENDPATH**/ ?>
