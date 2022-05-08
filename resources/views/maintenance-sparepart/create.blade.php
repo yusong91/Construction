00a382a4aac8ff5ae4c079091ff5a902593fcc4d
HOUN YOUSONG <yusonghoun91@gmail.com>
2022-05-08 10:43:27 +0700

    edit filesystem

--- resources/views/maintenance-sparepart/create.blade.php
+++ resources/views/maintenance-sparepart/create.blade.php
@@ -63,7 +63,7 @@
 
         <fieldset class="border p-2 " style="overflow-x: scroll;">
 
-            <legend>New Movement & Rent</legend>
+            <legend>New Maintenance</legend>
         
             <div class="row">
                 
