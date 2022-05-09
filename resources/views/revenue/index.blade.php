4fe11af7de3fd6e9dda94afa430e021c7a993612
HOUN YOUSONG <yusonghoun91@gmail.com>
2022-05-09 09:33:48 +0700

    add search and create button in revenue

--- resources/views/revenue/index.blade.php
+++ resources/views/revenue/index.blade.php
@@ -36,24 +36,22 @@
                     <div class="input-group custom-search-form">
                         <input  type="text" class="form-control input-solid" name="search" value="{{ Request::get('search') }}" placeholder="Search"/>
 
-                            <span class="input-group-append">
-                                @if (Request::has('search') && Request::get('search') != '')
-                                    <a href="{{ route('revenue.index') }}" class="btn btn-light d-flex align-items-center" role="button"> <i class="fas fa-times text-muted"></i> </a>
-                                                @endif
-                                        <button class="btn btn-light" type="submit" id="search-activities-btn">
-                                            <i class="fas fa-search text-muted"></i>
-                                        </button>
-                            </span>
-                        </div>
+                        <span class="input-group-append">
+                            @if (Request::has('search') && Request::get('search') != '')
+                                <a href="{{ route('revenue.index') }}" class="btn btn-light d-flex align-items-center" role="button"> <i class="fas fa-times text-muted"></i> </a>
+                            @endif
+                            <button class="btn btn-light" type="submit" id="search-activities-btn"><i class="fas fa-search text-muted"></i></button>
+                        </span>
+                    </div>
 
-                        {{ csrf_field() }}
+                    {{ csrf_field() }} 
                 </form>
             </div> 
             <div class="col-8">
 
                 <div class="float-right">
                     <div class="row">
-                        <div class="col-xl-5 col-md-5">
+                        <div class="col-xl-5">
                             <a href="{{ route('revenue.create') }}" class="btn btn-primary" style="width: 125px;" >+ Revenue</a>
                         </div>
                     </div> 
--- resources/views/revenue/test.blade.php
+++ resources/views/revenue/test.blade.php
@@ -29,15 +29,43 @@
                            id="authentication-tab" data-toggle="tab" href="#login-details" role="tab" aria-controls="home" aria-selected="true"> @lang('Customer')
                         </a>
                     </li>
-                   
                 </ul>
 
                 <div class="tab-content mt-4" id="nav-tabContent">
                     <div class="tab-pane fade show active px-2" id="details" role="tabpanel" aria-labelledby="nav-home-tab">
                         
+                        <div class="row mb-4">
+                            <div class="col-4">
+                                        
+                                <form action="" method="GET" class="border-bottom-light"  accept-charset="UTF-8">
+                                    <div class="input-group custom-search-form">
+                                        <input  type="text" class="form-control input-solid" name="search" value="{{ Request::get('search') }}" placeholder="Search"/>
+
+                                        <span class="input-group-append">
+                                            @if (Request::has('search') && Request::get('search') != '')
+                                                <a href="{{ route('revenue.index') }}" class="btn btn-light d-flex align-items-center" role="button"> <i class="fas fa-times text-muted"></i> </a>
+                                            @endif
+                                            <button class="btn btn-light" type="submit" id="search-activities-btn"><i class="fas fa-search text-muted"></i></button>
+                                        </span>
+                                    </div>
+
+                                    {{ csrf_field() }} 
+                                </form>
+                            </div> 
+                            <div class="col-8">
+
+                                <div class="float-right">
+                                    <div class="row">
+                                        <div class="col-xl-5">
+                                            <a href="{{ route('revenue.create') }}" class="btn btn-primary" style="width: 115px;" >+ Revenue</a>
+                                        </div>
+                                    </div> 
+                                </div>      
+                            </div>
+                        </div>
+
                         @include('revenue.partials.row')    
                          
-                        
                         <nav aria-label="Page navigation example">
                             <ul class="pagination"> 
                                 <?php $page = $paginate->current_page; ?>
@@ -59,10 +87,8 @@
                     </div>
                 </div>
             </div>
-  
         </div>
     </div>
-
 </div>
 
 @stop
