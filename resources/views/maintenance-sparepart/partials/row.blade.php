d916e2fd9569403ef6c5e2dd45713caf45650832
HOUN YOUSONG <yusonghoun91@gmail.com>
2022-06-04 16:23:04 +0700

    edit maintenacne

--- resources/views/maintenance-sparepart/partials/row.blade.php
+++ resources/views/maintenance-sparepart/partials/row.blade.php
@@ -1,19 +1,19 @@
 <div class="table-responsive" style="padding-top: 40px;">
-    <table class="table table-borderless table-striped" style="width: 200%;">
+    <table class="table table-borderless table-striped" style="width: 170%;">
         <thead> 
-            <th class="text-center align-middle" style="width: 5%;">Date</th>
-            <th class="text-center align-middle" style="width: 10%;">Equipment ID</th>
-            <th class="text-center align-middle" style="width: 10%;">Spare-Part/Service</th>
-            <th class="text-center align-middle" style="width: 10%;">Quantity</th>
-            <th class="text-center align-middle" style="width: 10%;">Unit</th>
-            <th class="text-center align-middle" style="width: 10%;">Unit Price</th>
-            <th class="text-center align-middle" style="width: 10%;">Amount</th>
-            <th class="text-center align-middle" style="width: 10%;">Supplier Name</th>
-            <th class="text-center align-middle" style="width: 10%;">Responsible Person</th>
-            <th class="text-center align-middle" style="width: 20%;">Memo</th>
-            <th class="text-center align-middle" style="width: 10%;">Image of Broken</th>
-            <th class="text-center align-middle" style="width: 10%;">Image of Replacement</th> 
-            <th class="text-center align-middle">Action</th>  
+            <th class="text-center align-middle" style="width: 2%;">Date</th>
+            <th class="text-center align-middle" style="width: 5%;">Equipment ID</th>
+            <th class="text-center align-middle" style="width: 5%;">Spare-Part/Service</th>
+            <th class="text-center align-middle" style="width: 3%;">Quantity</th>
+            <th class="text-center align-middle" style="width: 3%;">Unit</th>
+            <th class="text-center align-middle" style="width: 3%;">Unit Price</th>
+            <th class="text-center align-middle" style="width: 3%;">Amount</th>
+            <th class="text-center align-middle" style="width: 5%;">Supplier Name</th>
+            <th class="text-center align-middle" style="width: 5%;">Responsible Person</th>
+            <th class="text-center align-middle" style="width: 8%;">Memo</th>
+            <th class="text-center align-middle" style="width: 3%;">Image of Broken</th>
+            <th class="text-center align-middle" style="width: 3%;">Image of Replacement</th> 
+            <th class="text-center align-middle" style="width: 2%;">Action</th>  
         </thead>
         <tbody> 
             @if(count($maintenances))
@@ -32,7 +32,7 @@
                         <td class="text-center align-middle">{{ $item->note }}</td>
                         <td class="text-center align-middle" ><img src="{{ $item->image_broken ? getUrl($item->image_broken) : url('assets/img/no_photo.png') }}" width="80"></td>
                         <td class="text-center align-middle" ><img src="{{ $item->image_replace ? getUrl($item->image_replace) : url('assets/img/no_photo.png') }}" width="80"></td>   
-                        <td class="align-middle">        
+                        <td class="text-center align-middle ">        
                             <a href="{{ route('maintenance.edit', $item->id) }}" class="btn btn-icon edit" title="Update" data-toggle="tooltip" data-placement="top"> <i class="fas fa-edit"></i> </a>
                             <a href="{{ route('maintenance.destroy', $item->id) }}" class="btn btn-icon" data-action="" title="Delete" data-toggle="tooltip" data-placement="top" data-method="DELETE"data-confirm-title="@lang('app.please_confirm')" data-confirm-text="@lang('app.confirm_delete')" data-confirm-delete="@lang('app.yes_proceed')"><i class="fas fa-trash"></i></a>
                         </td>
